2b6fdc7fbf393413c80da4a5ab5ef5e7c7fbb76a
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-06-24 21:04:08 +0530

    updated bmi program

--- bmicalculator/bmi.cs
+++ bmicalculator/bmi.cs
@@ -44,7 +44,7 @@ namespace bmicalculator
             { Console.WriteLine("-> healthy"); }
 
             if ( Bmi_Calc >= 25)
-            { Console.WriteLine("-> Overweight"); }
+            { Console.WriteLine("-> Overweight");  }
 
         }
     }
