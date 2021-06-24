6500a1b33fead19b68985681b4c6e275415d12b4
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-06-24 20:09:25 +0530

    updated bmi program

---
+++ bmicalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace bmicalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMyBmi bmi = new TestMyBmi();
+            bmi.CalculateBmi();
+        }
+    }
+}
