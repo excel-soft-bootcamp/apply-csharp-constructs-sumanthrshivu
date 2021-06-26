ec0d6bd89df8f1b124f3ff0ea34f88a354946a37
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-06-26 15:07:56 +0530

    Inheritance Program

---
+++ Inheritance1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InheritanceProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students stf_details = new Students("MCA", "sumanth4206");
+        }
+    }
+}
