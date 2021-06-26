a5241856bd6a8738e0b3ca0c71c224938fce1b24
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-06-26 14:52:39 +0530

    Inheritance program

--- bmicalculator/Program.cs
+++ bmicalculator/Program.cs
@@ -1,22 +1,12 @@
 ﻿using System;
 
-namespace BmiCalculator
+namespace InheritanceProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            TestMyBmi bmi = new TestMyBmi();
-
-            Console.Write("Weight in kg: ");
-            bmi.Weight_Kg = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Height in meters: ");
-            bmi.Height_M = Convert.ToDouble(Console.ReadLine());
-
-
-            bmi.CalculateBmi();
-            bmi.ValidateBmi();
+            Students stf_details = new Students("MCA", "sumanth4206");
         }
     }
 }
