8a772a71b1d23cfce1fa6d4935926d3a12bde200
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-07-08 17:00:08 +0530

    updated BmiLibApp

---
+++ BmiCalculationLibApp/ReadFromConsoleLib/ReadFromConsoleType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFromConsoleLib
+{
+    public class ReadFromConsoleType
+    {
+        public double ConsoleReader(string message)
+        {
+            Console.WriteLine(message);
+            double value = Convert.ToDouble(Console.ReadLine());
+            return value;
+        }
+    }
+}
