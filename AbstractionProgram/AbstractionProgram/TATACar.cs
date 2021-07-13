51883ab473ea88d344c64c099aa22a9524cb7e94
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-07-13 19:23:37 +0530

    TATACar AbstractionProgram

---
+++ AbstractionProgram/AbstractionProgram/TATACar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractionProgram
+{
+    public class TATACar
+    {
+        IEngine _engine =null;
+
+        public TATACar(IEngine engine)
+        {
+             this._engine= engine;
+        }
+        public void Drive()
+        {
+
+            _engine.Start();
+        }
+        public void Halt()
+        {
+
+            _engine.Stop();
+        }
+    }
+}
---
+++ AbstractionProgram/AbstractionProgram/VeriCorEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractionProgram
+{
+    public class VeriCorEngine : IEngine
+    {
+        public void Start() { }
+        public void Stop() { }
+
+    }
+}
