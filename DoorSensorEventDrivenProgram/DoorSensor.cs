504e8c5e7540a749f24c97a9c96c41519c8fbf69
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-07-13 17:56:35 +0530

    EventDrivenProgram

---
+++ DoorSensorEventDrivenProgram/DoorSensor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorSensorEventDrivenProgram
+{
+    public delegate void DoorSensorstatusHandler(string status);
+   
+    public class DoorSensor
+    {
+
+        string DoorSensorstatus;
+        public event DoorSensorstatusHandler OnDoorSensorStatusChanged;
+        public void Open()
+        {
+            DoorSensorstatus = "DoorOpened";
+            this.Notify();
+        }
+        public void Close()
+        {
+            DoorSensorstatus = "DoorClosed";
+            this.Notify();
+        }
+        void Notify()
+        {
+            string message = $"{this.DoorSensorstatus} and Time : {System.DateTime.Now.ToString()}";
+            OnDoorSensorStatusChanged.Invoke(this.DoorSensorstatus);
+            OnDoorSensorStatusChanged.Invoke(message);
+        }
+
+
+    }
+}
