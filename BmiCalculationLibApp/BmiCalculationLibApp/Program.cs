fc71504c40cced9baba77c82653ba3708dd7ba61
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-07-13 21:32:43 +0530

    Update Program.cs

--- BmiCalculationLibApp/BmiCalculationLibApp/Program.cs
+++ BmiCalculationLibApp/BmiCalculationLibApp/Program.cs
@@ -14,60 +14,62 @@ namespace BmiCalculationLibApp
     }
     class Program
     {
-       //static int count = 3;
        
         static void Main(string[] args)
         {
-            //do
-            //{
+            int count = 3;
+            do
+            {
                 //Options _choice = default(Options);
                 string displayMsg = $"Enter Your Choice {(int)Options.ConsoleInput}->ConsoleInput,{(int)Options.FileInput}-->FileInput";
                 ConsoleDisplayLib.ConsoleDisplayType.DisplayMessage(displayMsg);
-                Options _choice = (Options)Convert.ToDouble(ConsoleDisplayLib.ConsoleDisplayType.ReadFromConsole());
-                    //    try
-                    //    {
-                    //        double optionValue = Convert.ToDouble(ConsoleDisplayLib.ConsoleDisplayType.ReadFromConsole());
-
-                    //        if (optionValue < 1 || optionValue > 2)
-                    //        {
-                    //            Console.WriteLine("invalid options");
-                    //        }
-                    //        else
-                    //        {
-                    //            break;
-                    //        }
-                    //    }
-
-                    //    catch (FormatException ex)
-                    //    {
-                    //        Console.WriteLine("choosed options must be number");
-                    //    }
-                    //    --count;
-                    //} while (count > 0);
-            switch (_choice)
+                
+            try
             {
-                case Options.ConsoleInput:
-                    UserInputLib.UserInputType.UserInput();
-                    break;
+                    Options _choice = (Options)Convert.ToInt32(ConsoleDisplayLib.ConsoleDisplayType.ReadFromConsole());
 
-                case Options.FileInput:
-                    try
-                    {
-                        UserInputLib.UserInputType.CsvInputToBmiCalculator();
-                    }
+                    if (_choice < (Options)1 || _choice > (Options)2)
+                {
+                    Console.WriteLine("invalid options");
+                }
+                else
+                {
+                        switch (_choice)
+                        {
+                            case Options.ConsoleInput:
+                                UserInputLib.UserInputType.UserInput();
+                                Console.ReadKey();
+                                break;
 
-                    catch (FileNotFoundException e)
-                    { 
-                        Console.WriteLine("Could Not Find File" + e);  
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Please provide valid type" + ex);
-                    }
-                    Console.ReadKey();
-                    break;
-           
+                            case Options.FileInput:
+                                try
+                                {
+                                    UserInputLib.UserInputType.CsvInputToBmiCalculator();
+                                }
+
+                                catch (FileNotFoundException e)
+                                {
+                                    Console.WriteLine("Could Not Find File" + e);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("Please provide valid type" + ex);
+                                }
+                                Console.ReadKey();
+                                break;
+
+                        }
+                        break;
+                }
             }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("choosed options must be number");
+            }
+            --count;
+        } while (count > 0);
+            
         }
     }
 }
